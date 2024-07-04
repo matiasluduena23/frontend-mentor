918b8e3d74536ad716514723eefe6082c4278197
Matias Ludueña <matiasluduena23@gmail.com>
2024-07-04 16:24:19 -0300

    09-nft-preview-card-component

--- newbie/09-nft-preview-card-component/tailwind.config.js
+++ newbie/09-nft-preview-card-component/tailwind.config.js
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./build/*.html'],
-    theme: {
-        extend: {
-            fontFamily: {
-                young: 'Young Serif',
-                outfit: 'Outfit',
-            },
-            colors: {
-                clRoseWhite: '#fff5fa',
-                clEggshell: '#f3e6d8',
-                clLightGrey: '#e4ded8',
-                clWenge: '#5f574e',
-                clDark: '#302d2c',
-                clNutmag: '#854632',
-                clRasberry: '#7b284f',
-            },
-        },
-    },
-    plugins: [],
+	content: ['./build/*.html'],
+	theme: {
+		extend: {
+			fontFamily: {
+				outfit: 'Outfit',
+			},
+			colors: {
+				clSoftBlue: 'hsl(215, 51%, 70%)',
+				clCyan: 'hsl(178, 100%, 50%)',
+				clDarkBlue: 'hsl(217, 54%, 11%)',
+				clVeryDarkBlue: 'hsl(216, 50%, 16%)',
+				clLime: 'hsl(215, 32%, 27%)',
+				clDark: '#302d2c',
+			},
+		},
+	},
+	plugins: [],
 };
